fc7434667cd5ca4132d1d850d81319b50083b1da
Spizium <pojat.pelle2@gmail.com>
2026-01-19 13:20:31 +0200

    Update SW1_M5Q1.py
    Made the piece of code actually functional lmao

--- SW_module_5/SW1_M5Q1.py
+++ SW_module_5/SW1_M5Q1.py
@@ -1,15 +1,9 @@
 import random
 
 rolls = int(input("How many dice to roll: "))
-rolled = []
-dice = int(0)
+dicesum = int(0)
 
 for n in range(rolls):
-    dice = random.randint(1, 6)
-    rolled.append(dice)
-
-dicesum = int(0)
-for rolled in range(rolls-1):
-    dicesum += n
+    dicesum += random.randint(1,6)
 
 print(f"Sum of the dice: {dicesum}")
