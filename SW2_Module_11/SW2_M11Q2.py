473140719f6a0638cb98c298ad6cbfb537d557e6
Spizium <pojat.pelle2@gmail.com>
2026-03-22 18:38:37 +0200

    Module 11 commitz
    Added Module 11 sh#tstuff

---
+++ SW2_Module_11/SW2_M11Q2.py
@@ -0,0 +1,26 @@
+class Car:
+    def __init__(self, license_plate, maximum_speed):
+        self.license_plate = license_plate
+        self.maximum_speed = maximum_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+
+    def accelerate(self, change):
+        self.current_speed += change
+        if self.current_speed > self.maximum_speed:
+            self.current_speed = self.maximum_speed
+        if self.current_speed < 0:
+            self.current_speed = 0
+
+    def drive(self, hours):
+        self.travelled_distance += self.current_speed * hours
+
+class ElectricCar(Car):
+    def __init__(self, license_plate, maximum_speed, bat_cap):
+        super().__init__(license_plate, maximum_speed,)
+        self.battery_capacity = bat_cap
+
+class GasolineCar(Car):
+    def __init__(self, license_plate, maximum_speed, tank_volume):
+        super().__init__(license_plate, maximum_speed,)
+        self.tank_volume = tank_volume
